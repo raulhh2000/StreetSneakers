bca1755243f48fce78f09179529e433a44d6b343
Albertomartin21 <alberto_martin_21@hotmail.com>
2022-02-03 12:22:44 +0100

    Create Controller Home

---
+++ StreetSneakers/src/main/java/urjc/dad/controllers/HomeController.java
@@ -0,0 +1,29 @@
+package urjc.dad.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import urjc.dad.models.Product;
+import urjc.dad.repositories.ProductRepository;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+	@Autowired
+	ProductRepository productRepository;
+	
+	@GetMapping("")
+	public String showHome(Model model) {
+		List<Product> list=productRepository.findAll()
+		model.addAttribute("products", list);
+	    return "list_template";
+	}
+	 
+	
+}
