844fbbf39d32c84613b23b9ca37bf2483dabcb2c
raulhh2000 <raulitohht00@gmail.com>
2022-03-15 13:57:19 +0100

    chore: Add the InternalService application

---
+++ InternalService/src/test/java/urjc/dad/InternalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package urjc.dad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
