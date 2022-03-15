6f6e887e5b2e739c732e7cc10e25b1d8de5f28d3
raulhh2000 <raulitohht00@gmail.com>
2022-03-15 14:07:22 +0100

    feat: Add EmailService.java

---
+++ InternalService/src/main/java/urjc/dad/service/EmailService.java
@@ -0,0 +1,22 @@
+package urjc.dad.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendEmail(String to, String subject, String content) {
+        SimpleMailMessage email = new SimpleMailMessage();
+
+        email.setTo(to);
+        email.setSubject(subject);
+        email.setText(content);
+
+        mailSender.send(email);
+    }
+}
