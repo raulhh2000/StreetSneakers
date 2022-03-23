eb63f9e2d8caea66ad7a98337218b94266dba610
Albertomartin21 <alberto_martin_21@hotmail.com>
2022-03-23 23:56:05 +0100

    refact: Format the pdf

--- InternalService/src/main/java/urjc/dad/service/PDFService.java
+++ InternalService/src/main/java/urjc/dad/service/PDFService.java
@@ -6,11 +6,16 @@ import java.io.IOException;
 
 import org.springframework.stereotype.Service;
 
+import com.lowagie.text.Cell;
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
+import com.lowagie.text.Element;
 import com.lowagie.text.Font;
 import com.lowagie.text.Paragraph;
 import com.lowagie.text.Phrase;
+import com.lowagie.text.Table;
+import com.lowagie.text.alignment.HorizontalAlignment;
+import com.lowagie.text.alignment.VerticalAlignment;
 import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
@@ -26,18 +31,63 @@ public class PDFService {
 			Document document = new Document();
 			PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(namePDF));
 			Font font = new Font(Font.HELVETICA, 16, Font.BOLDITALIC, Color.BLACK);
+			Font font2 = new Font(Font.HELVETICA, 16, Font.ITALIC, Color.BLACK);
+		
+			Font font3 = new Font(Font.HELVETICA, 16, Font.ITALIC, new Color(230,88,61));
+			Font font4 = new Font(Font.HELVETICA, 16, Font.BOLDITALIC, Color.WHITE);
 			document.open();
-			Paragraph paragraph0 = new Paragraph("DATOS DEL PEDIDO: ", font);
+			
+			PdfPTable table = new PdfPTable(1); 
+			
+			PdfPCell cell = new PdfPCell(new Phrase("FACTURA SIMPLIFICADA", font4));
+		    cell.setVerticalAlignment(Element.ALIGN_CENTER);
+		    cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+		    cell.setBackgroundColor(new Color(45,62,80));
+			
+		    table.addCell(cell);
+			
+			Paragraph paragraph0 = new Paragraph("DATOS DEL CLIENTE: ", font);
 			Paragraph paragraph12 = new Paragraph(" ", font);
-			Paragraph paragraph = new Paragraph("Nombre: " + dataPurchase.getName(), font);
-			Paragraph paragraph1 = new Paragraph("Apellido: " + dataPurchase.getLastName(), font);
-			Paragraph paragraph2 = new Paragraph("Correo electronico: " + dataPurchase.getEmail(), font);
-			Paragraph paragraph3 = new Paragraph("Telefono: " + dataPurchase.getPhone(), font);
-			Paragraph paragraph4 = new Paragraph("Dirección: " + dataPurchase.getAddress(), font);
-			Paragraph paragraph5 = new Paragraph("Cuenta Bancaria: " + dataPurchase.getBankAccount(), font);
-			Paragraph paragraph6 = new Paragraph("Fecha del pedido: " + dataPurchase.getDate(), font);
-			Paragraph paragraph7 = new Paragraph("Precio total: " + dataPurchase.getTotalPrice(), font);
-			Paragraph paragraph8 = new Paragraph("Número de productos: " + dataPurchase.getNumProducts(), font);
+			
+			Paragraph paragraph = new Paragraph();
+			paragraph.add(new Phrase("Nombre: ",font));
+			paragraph.add(new Phrase(dataPurchase.getName(), font2));
+			
+			
+			Paragraph paragraph1 = new Paragraph();
+			paragraph1.add(new Phrase("Apellido: " ,font));
+			paragraph1.add(new Phrase(dataPurchase.getLastName(), font2));
+			
+			Paragraph paragraph2 = new Paragraph();
+			paragraph2.add(new Phrase("Correo electronico: ",font));
+			paragraph2.add(new Phrase(dataPurchase.getEmail(), font2));
+			
+			Paragraph paragraph3 = new Paragraph();
+			paragraph3.add(new Phrase("Telefono: ",font));
+			paragraph3.add(new Phrase(dataPurchase.getPhone(), font2));
+			
+			Paragraph paragraph4 = new Paragraph();
+			paragraph4.add(new Phrase("Dirección: ",font));
+			paragraph4.add(new Phrase(dataPurchase.getAddress(), font2));
+			
+			Paragraph paragraph5 = new Paragraph();
+			paragraph5.add(new Phrase("Cuenta Bancaria: ",font));
+			paragraph5.add(new Phrase(dataPurchase.getBankAccount(), font2));
+			
+			Paragraph paragraph6 = new Paragraph();
+			paragraph6.add(new Phrase("Fecha del pedido: ",font));
+			paragraph6.add(new Phrase(dataPurchase.getDate(), font2));
+			
+			Paragraph paragraph7 = new Paragraph();
+			paragraph7.add(new Phrase("Precio total: ",font));
+			paragraph7.add(new Phrase(Double.toString(dataPurchase.getTotalPrice())+"€", font3));
+			
+			
+			Paragraph paragraph8 = new Paragraph();
+			paragraph8.add(new Phrase("Número de productos: ",font));
+			paragraph8.add(new Phrase(Integer.toString(dataPurchase.getNumProducts()), font2));
+			
+			
 			Paragraph paragraph9 = new Paragraph(" ", font);
 			Paragraph paragraph10 = new Paragraph("PRODUCTOS: ", font);
 			Paragraph paragraph11 = new Paragraph(" ", font);
@@ -50,8 +100,10 @@ public class PDFService {
 		    	tablaLineItem.addCell(item.getName());
 		    	tablaLineItem.addCell(item.getBrand());
 		    	tablaLineItem.addCell(Double.toString(item.getSize()));
-		    	tablaLineItem.addCell(Double.toString(item.getPrice()));
+		    	tablaLineItem.addCell(Double.toString(item.getPrice())+"€");
 		      }
+		    document.add(table);
+		    document.add(paragraph12);
 		    document.add(paragraph0);
 		    document.add(paragraph12);
 			document.add(paragraph);
@@ -61,12 +113,13 @@ public class PDFService {
 			document.add(paragraph4);
 			document.add(paragraph5);
 			document.add(paragraph6);
-			document.add(paragraph7);
-			document.add(paragraph8);
 			document.add(paragraph9);
 			document.add(paragraph10);
 			document.add(paragraph11);
 			document.add(tablaLineItem);
+			document.add(paragraph12);
+			document.add(paragraph8);
+			document.add(paragraph7);
 			document.close();
 			writer.close();
 		} catch (DocumentException | IOException de) {
