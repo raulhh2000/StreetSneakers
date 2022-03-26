1c2d4beb3a23e66fef9935bf94966264526f2c40
ezi2000 <1d.fatima.smounat.@gmail.com>
2022-03-26 17:25:35 +0100

    refactor: Add concurrence to EmailController

--- InternalService/src/main/java/urjc/dad/api/EmailController.java
+++ InternalService/src/main/java/urjc/dad/api/EmailController.java
@@ -1,5 +1,8 @@
 package urjc.dad.api;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,14 +23,20 @@ public class EmailController {
 
 	@PostMapping("/email/send")
 	public ResponseEntity<String> send(@RequestBody(required=true)Email email) {
-		emailService.sendEmail(email.getTo(), email.getSubject(), email.getContent());
+		ExecutorService executor = Executors.newFixedThreadPool(1);
+		executor.execute(() -> emailService.sendEmail(email.getTo(), email.getSubject(), email.getContent()));
+		executor.shutdown();
 		return ResponseEntity.ok("Se ha enviado el correo electronico");
 	}
 	
 	@PostMapping("/email/sendPDF")
 	public ResponseEntity<String> sendPDF(@RequestBody(required=true)Email email){
-		String namePDF = pdfService.createPdf(email.getDataPurchase());
-		emailService.sendEmail(email.getTo(), email.getSubject(), email.getContent(), namePDF);
+		ExecutorService executor = Executors.newFixedThreadPool(1);
+		executor.execute(() -> {
+			String namePDF = pdfService.createPdf(email.getDataPurchase());
+			emailService.sendEmail(email.getTo(), email.getSubject(), email.getContent(), namePDF);
+		});
+		executor.shutdown();
 		return ResponseEntity.ok("Se ha enviado el correo electronico");
 	}
 }
