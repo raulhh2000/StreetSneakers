c2014ee40b4447c22bb33f8ed8c9f54dd0494fa8
ezi2000 <1d.fatima.smounat.@gmail.com>
2022-05-03 23:33:12 +0200

    feat: Cache update from Hazelcast to redis

--- StreetSneakers/src/main/java/urjc/dad/StreetSneakersApplication.java
+++ StreetSneakers/src/main/java/urjc/dad/StreetSneakersApplication.java
@@ -1,45 +1,17 @@
 package urjc.dad;
 
-import java.util.Arrays;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-import com.hazelcast.config.Config;
-import com.hazelcast.config.JoinConfig;
-import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.spring.cache.HazelcastCacheManager;
-
-@EnableHazelcastHttpSession
+@EnableRedisHttpSession
 @EnableCaching
 @SpringBootApplication
 public class StreetSneakersApplication {
 
-	private static final Log LOG = LogFactory.getLog(StreetSneakersApplication.class);
-	
 	public static void main(String[] args) {
 		SpringApplication.run(StreetSneakersApplication.class, args);
 	}
 	
-	@Bean
-	public CacheManager cacheManager(HazelcastInstance hazelcastInstance) {
-		LOG.info("Activating cache...");
-		return new HazelcastCacheManager(hazelcastInstance);
-	}
-	
-	@Bean
-	public Config config() {
-		Config config = new Config();
-		JoinConfig joinConfig = config.getNetworkConfig().getJoin();
-		joinConfig.getMulticastConfig().setEnabled(false);
-		joinConfig.getTcpIpConfig().setEnabled(true).setMembers(Arrays.asList("app1","app2"));
-		return config;
-	}
-
 }
