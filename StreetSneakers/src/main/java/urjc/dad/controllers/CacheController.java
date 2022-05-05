813720cb73a5072eba53b3e7922f600ded71083a
ezi2000 <1d.fatima.smounat.@gmail.com>
2022-05-05 11:40:11 +0200

    refactor: Update CacheController to work with redis

--- StreetSneakers/src/main/java/urjc/dad/controllers/CacheController.java
+++ StreetSneakers/src/main/java/urjc/dad/controllers/CacheController.java
@@ -1,27 +1,48 @@
 package urjc.dad.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.CacheManager;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import redis.clients.jedis.Jedis;
 
 @RestController
 public class CacheController {
 
-	@Autowired
-	private CacheManager cacheManager;
+	@Value("${spring.redis.host}")
+	private String redisHost;
+	@Value("${spring.redis.port}")
+	private int redisPort;
+	@Value("${spring.redis.password}")
+	private String redisPassword;
 
 	@GetMapping("/cache")
-	public List<Map<Object, Object>> getCacheContent() {
-		
-		List<Map<Object, Object>> listCaches= new ArrayList<>();
-		for(String cacheName: cacheManager.getCacheNames()) {
-			listCaches.add((Map<Object, Object>) cacheManager.getCache(cacheName).getNativeCache());
-		}
-		return listCaches;
+	public Set<String> getKeys() {
+		Jedis jedis = new Jedis(redisHost, redisPort);
+		jedis.auth(redisPassword);
+		Set<String> keysResult = jedis.keys("*");
+		jedis.close();
+		return keysResult;
+	}
+
+	@GetMapping("/cache/{key}")
+	public Set<String> getKeys(@PathVariable String key) {
+		Jedis jedis = new Jedis(redisHost, redisPort);
+		jedis.auth(redisPassword);
+		Set<String> keysResult = jedis.keys(key + ":*");
+		jedis.close();
+		return keysResult;
+	}
+
+	@GetMapping("/cache/key/{key}")
+	public String getCacheContent(@PathVariable String key) {
+		Jedis jedis = new Jedis(redisHost, redisPort);
+		jedis.auth(redisPassword);
+		String keysResult = jedis.get(key);
+		jedis.close();
+		return keysResult;
 	}
 }
